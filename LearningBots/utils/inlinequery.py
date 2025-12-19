25fd8f1d3429020fae05b42a01f4f44fc0d0a747
Zawskillearning <143388118+Zawskillearning@users.noreply.github.com>
2025-12-20 01:27:51 +0630

    Update inlinequery.py

--- LearningBots/utils/inlinequery.py
+++ LearningBots/utils/inlinequery.py
@@ -7,19 +7,19 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://files.catbox.moe/10xn4h.jpg",
+            thumb_url="https://image.zaw-myo.workers.dev/image/69756ba3-35e2-475a-934f-830bdbf73e97",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://files.catbox.moe/10xn4h.jpg",
+            thumb_url="https://image.zaw-myo.workers.dev/image/2c9c2a24-20d1-4ac7-ac01-a44ee34ffe83",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
-            thumb_url="https://files.catbox.moe/10xn4h.jpg",
+            thumb_url="https://image.zaw-myo.workers.dev/image/55f2147e-931e-4349-a714-907cc1d7be64",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
@@ -31,13 +31,13 @@ answer.extend(
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
-            thumb_url="https://files.catbox.moe/10xn4h.jpg",
+            thumb_url="https://image.zaw-myo.workers.dev/image/32a96366-b1c0-4371-a305-5a893045e358",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://files.catbox.moe/10xn4h.jpg",
+            thumb_url="https://image.zaw-myo.workers.dev/image/32a96366-b1c0-4371-a305-5a893045e358",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
